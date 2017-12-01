4f15f95d643e60162882fdf1657147176209e176
a-stanislawski <33346023+a-stanislawski@users.noreply.github.com>
2017-12-01 11:45:53 +0100

    removed unused lines

--- src/init.sh
+++ src/init.sh
@@ -16,33 +16,18 @@ sleep 2
 MOTOR=$!
 sleep 5
 
-# start voice recognition
-#sudo /root/pocketsphinx-5prealpha/src/programs/pocketsphinx_continuous -lm $LM -dict $DICT -inmic yes >> $SPATH/com &
-
 # reading moves
 $SPATH/audio > $SPATH/logs/audio &
 sleep 2
 
-#$SPATH/audio2 > $SPATH/logs/audio2 &
-
-
 #export data to read voltage
 echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
-
 sleep 2
 
-
-# check if main program is running
-#while ! ifconfig wlan0 > /dev/null 2> /dev/null ; do
-	#/sbin/ifdown wlan0
-#	sleep 2
-#	echo 111
-#done
-
-#echo 222
-#/sbin/ifup wlan0
+#update date and time
 sleep 3
 ntpdate -b -s -u pool.ntp.org
+
 
 #echo "Connection lost" | festival --tts
 
