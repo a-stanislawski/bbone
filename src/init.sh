0e1d35bab292f3acd607e4b6c14566e3ce556821
a-stanislawski <33346023+a-stanislawski@users.noreply.github.com>
2017-12-01 11:44:18 +0100

    Update init.sh

--- src/init.sh
+++ src/init.sh
@@ -1,43 +1,49 @@
 #!/bin/bash
 
-
 SCRIPT=$(readlink -f "$0")
-# Absolute path this script is in
+# Absolute path this script
 SPATH=$(dirname "$SCRIPT")
 
+# paths for dictionary
+#DICT="$SPATH/dict/9416.dic"
+#LM="$SPATH/dict/9416.lm"
 
-DICT="$SPATH/dict/9416.dic"
-LM="$SPATH/dict/9416.lm"
-
+#start motor control
 $SPATH/testbbb > $SPATH/logs/motor &
 sleep 2
+
+# remember process PID to control its state
 MOTOR=$!
 sleep 5
-sudo /root/pocketsphinx-5prealpha/src/programs/pocketsphinx_continuous -lm $LM -dict $DICT -inmic yes >> $SPATH/com &
 
-sleep 6
+# start voice recognition
+#sudo /root/pocketsphinx-5prealpha/src/programs/pocketsphinx_continuous -lm $LM -dict $DICT -inmic yes >> $SPATH/com &
 
+# reading moves
 $SPATH/audio > $SPATH/logs/audio &
 sleep 2
 
-
-#ok
-$SPATH/audio2 > $SPATH/logs/audio2 &
-
-#$SPATH/audio > $SPATH/logs/audio &
-
-#~/BTled/
-#~/BTled/audio3 > ~/logaudio3 &
+#$SPATH/audio2 > $SPATH/logs/audio2 &
 
 
-date >> logs/script
-echo -e " completed\n" >> logs/script
+#export data to read voltage
+echo cape-bone-iio > /sys/devices/bone_capemgr.9/slots
 
-while(ps -e | grep $MOTOR) do
-	sleep 2
-done
+sleep 2
 
-echo "Connection lost" | festival --tts
+
+# check if main program is running
+#while ! ifconfig wlan0 > /dev/null 2> /dev/null ; do
+	#/sbin/ifdown wlan0
+#	sleep 2
+#	echo 111
+#done
+
+#echo 222
+#/sbin/ifup wlan0
+sleep 3
+ntpdate -b -s -u pool.ntp.org
+
+#echo "Connection lost" | festival --tts
 
 sleep 3
-
